40d1cc4b7954dc31538b846f956050ac25d44924
Carson Breissinger <carson@breissinger.net>
2026-01-06 20:33:17 -0500

    Sending to kernel
    Sends completely useless data that the kernel cannot understand.
    Sucessfully reaches kernel.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use jupyter_protocol::{ExecuteRequest, JupyterMessage, JupyterMessageContent};
+use serde_json::Value;
+use std::fs;
+use zmq::SocketType::{self, DEALER};
+
+// TODO: Separate out this main function. Probably will want a lib.rs file eventually
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Spinning up manual kernel and hardcoding json filepath for now
+    let contents = fs::read_to_string(
+        "/home/carso/.local/share/jupyter/runtime/kernel-c7565892-0c0a-428e-8289-f8008083e080.json",
+    )
+    .expect("File read failed");
+
+    // Extract kernel communication info from json (move this to a function)
+    let json: Value = serde_json::from_str(&contents)?;
+    let ip = json["ip"].as_str().unwrap();
+    let shell_port = json["shell_port"].as_u64().unwrap();
+
+    // Set up zmq sockets
+    let ctx = zmq::Context::new();
+    let shell_dealer = ctx.socket(SocketType::DEALER)?;
+
+    let endpoint = format!("tcp://{ip}:{shell_port}");
+    shell_dealer.connect(&endpoint)?;
+
+    // Send a msg
+    shell_dealer.send("print('Hello, World!')", 0)?;
+
+    Ok(())
 }
